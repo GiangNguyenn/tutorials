3e96a9016fc3ef26247338cec6f3ef4048b17f06
GiangNguyenn <giang.nguyen@skedulo.com>
2022-06-29 13:23:14 +0700

    Initial commit

---
+++ src/week1.java
@@ -0,0 +1,20 @@
+public class week1 {
+    public static void main(String[] args){
+
+//        Exercise 1
+        String name = "Nina";
+        String frame = "***********";
+        System.out.println(frame);
+        System.out.println("*  "+ name +"  *");
+        System.out.println(frame);
+
+//        Exercise2
+       int x= 10;
+       int y= 10;
+
+
+
+
+
+    }
+}
